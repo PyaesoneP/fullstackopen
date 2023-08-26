59f02671344de158a28486534040d3be85c8d760
Pyae Sone <pyaesone.perfect2014@gmail.com>
2023-08-26 17:43:01 +0800

    Create Display and Button components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,45 @@
 import { useState } from 'react'
 
+const Display = () => {
+return(
+  <>
+  <Button />
+  </>
+)
+}
+
+const Statistics = () => {
+
+}
+
+const StatisticLine = () => {
+
+}
+
+const Button = () => {
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return(
+    <>
+    <button onClick={handleGoodClick}>Good</button>
+    <button onClick={handleNeutralClick}>Neutral</button>
+    <button onClick={handleBadClick}>Bad</button>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([]);
 
   return (
     <div>
-      code here
+      <Display />
     </div>
   )
 }
