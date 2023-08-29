03ba17b3f2d434fffe149d9fdc21eb78a2db5c2a
Pyae Sone <pyaesone.perfect2014@gmail.com>
2023-08-29 13:28:28 +0800

    Set setAll state

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 
-const Display = (props) => {
+const Render = (props) => {
   const header = props.header
+  const text = props.text
 return(
   <>
   <Header header = {header}/>
@@ -10,6 +11,7 @@ return(
 }
 
 const Statistics = () => {
+
 
 }
 
@@ -26,25 +28,35 @@ const Button = (props) => {
 }
 
 const Header = (props) => <><h1>{props.header}</h1></>
+const Text = (props) => <><p>{props.text}</p></>
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allClicks, setAll] = useState([]);
+  const [allClicks, setAll] = useState({good: 0, neutral: 0, bad: 0});
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    setGood(good+1)
+    setAll({...allClicks, good: allClicks.good + 1})
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral+1)
+    setAll({...allClicks, neutral: allClicks.neutral + 1})
+  }
+  const handleBadClick = () => {
+    setBad(bad+1)
+    setAll({...allClicks, bad: allClicks.bad + 1})
+  }
 
   return (
     <div>
-      <Display header = 'give feedback'/>
+      <Render header = 'give feedback'/>
       <Button handleClick = {handleGoodClick} text = 'Good'/>
       <Button handleClick = {handleNeutralClick} text = 'Neutral'/>
       <Button handleClick = {handleBadClick} text = 'Bad'/>
-      <Display header = 'statistics'/>
+      <Render header = 'statistics'/>
     </div>
   )
 }
